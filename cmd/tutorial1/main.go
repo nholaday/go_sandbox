d4de9b2d91176a04d1f8d1a76397f02a9b956ac6
Nic Holaday <nic@storeease.com>
2025-09-25 05:52:24 -0700

    types

--- cmd/tutorial1/main.go
+++ cmd/tutorial1/main.go
@@ -4,6 +4,20 @@ import "fmt"
 func main(){
 	var myNum int
 	myNum = 15
+	// positive ints
+	var myUnsigned uint32 = 40000
+	var myFloat float32 // defaults to 0
 	fmt.Println("Hey first program!")
-	fmt.Println(myNum)
+	fmt.Println(myNum, myUnsigned, myFloat)
+
+
+	var myString string = "first \nstring"
+	var myString2 string = `string using backticks`
+	fmt.Println(myString, myString2)
+
+	var myRune rune = 'a' // single quotes for runes
+	fmt.Println(myRune)
+
+	var myBool bool = true
+	fmt.Println(myBool)
 }
