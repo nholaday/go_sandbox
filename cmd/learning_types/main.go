76bd3ded3f7246ca2e08ca53f0152be93df1d3e3
Nic Holaday <nholaday@gmail.com>
2025-10-02 10:19:59 -0700

    structs

---
+++ cmd/learning_types/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type student struct {
+	name string
+	age uint8
+	major string
+	// can add a struct to the type -- student.schoolInfo.name
+	schoolInfo school
+	// or can add the subfields directly the type -- student.year, student.paidOff
+	studentStatus
+}
+
+type school struct {
+	name string
+}
+
+type studentStatus struct {
+	year uint8
+	paidOff bool
+}
+
+// methods on structs (like private functions on a class)
+func (s student) yearsLeft() uint8 {
+	return 4 - s.year
+}
+
+func main(){
+	// uninitialized will have default type values
+	// "", 0, ""
+	var joe student
+	fmt.Println(joe.name, joe.age, joe.major)
+	
+	var tuBerlin school = school{"TU Berlin"}
+	// A lot like instantiating a class and calling methods on it
+	var mary student = student{"Mary", 39, "Japanese", tuBerlin, studentStatus{3, true}}
+	fmt.Println(mary.name, mary.age, mary.major, mary.schoolInfo.name, mary.year)
+	fmt.Println(mary.yearsLeft())
+	
+	// anonymous struct (not reusable)
+	var myEngine = struct{
+		mpg uint8
+		gallons uint8
+	}{25,15}
+	fmt.Println(myEngine.mpg, myEngine.gallons)
+}
